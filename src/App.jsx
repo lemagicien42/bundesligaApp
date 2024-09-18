b90bbfba85c836f6289032ba3591d8978ba468ef
lemagicien42 <totosuper40@gmail.com>
2024-09-18 12:02:47 +0200

    upgraden

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   const [matches, setMatches] = useState([]);
   const [team1, setTeam1] = useState("");
   const [team2, setTeam2] = useState("");
@@ -9,39 +9,47 @@ function App() {
 
   useEffect(() => {
     fetch("https://api.openligadb.de/getmatchdata/bl1/2024")
-      .then(response => response.json())
-      .then(data => setMatches(data))
-      .catch(error => console.error("Error", error));
+      .then((response) => response.json())
+      .then((data) => setMatches(data))
+      .catch((error) => console.error("Error", error));
   }, []);
 
   const handleSearch = () => {
-    const foundMatch = matches.find(match => 
-      (match.team1.teamName.toLowerCase().includes(team1.toLowerCase()) &&
-       match.team2.teamName.toLowerCase().includes(team2.toLowerCase())) ||
-      (match.team1.teamName.toLowerCase().includes(team2.toLowerCase()) &&
-       match.team2.teamName.toLowerCase().includes(team1.toLowerCase()))
+    const foundMatch = matches.find(
+      (match) =>
+        (match.team1.teamName.toLowerCase().includes(team1.toLowerCase()) &&
+          match.team2.teamName.toLowerCase().includes(team2.toLowerCase())) ||
+        (match.team1.teamName.toLowerCase().includes(team2.toLowerCase()) &&
+          match.team2.teamName.toLowerCase().includes(team1.toLowerCase()))
     );
-    
+
     if (foundMatch) {
       const homeTeam = foundMatch.team1.teamName;
       const awayTeam = foundMatch.team2.teamName;
-      const score = `${foundMatch.goals[1].scoreTeam1}:${foundMatch.goals[1].scoreTeam2}`;
-      const date = `${foundMatch.matchDateTime}`;
 
-      setResult(`Match found:
-      Date: ${date}\n
-      das Spiel findet in 
-      ${homeTeam} vs ${awayTeam}\n
-      Score: ${score}`);
-    } else {
-      setResult( " No match");
+      const date = `${foundMatch.matchDateTime}`;
+      if (foundMatch.goals.length > 0) {
+        const score = `${
+          foundMatch.goals[foundMatch.goals.length - 1].scoreTeam1
+        }:${foundMatch.goals[foundMatch.goals.length - 1].scoreTeam2}`;
+        setResult(`Match found:
+          Date: ${date}\n
+          das Spiel findet bei ${homeTeam} statt\n
+          ${homeTeam} vs ${awayTeam}\n
+          Score: ${score}`);
+      } else {
+        setResult(" No match");
+      }
     }
   };
 
   return (
     <>
-      <h1>Bundesliga Match Finder</h1>
-      <div className='inputStyle'>
+      <div className="header">
+        <h1>Bundesliga Match Finder </h1>
+        <img src="/public/bundesliga-seeklogo.svg" alt="" className="logo" />
+      </div>
+      <div className="inputStyle">
         <input
           type="text"
           value={team1}
@@ -54,12 +62,11 @@ function App() {
           onChange={(e) => setTeam2(e.target.value)}
           placeholder="Enter Second Team Name"
         />
-        <button onClick={handleSearch}>Search</button>
+        <button className="buttonStyle" onClick={handleSearch}>
+          Search
+        </button>
       </div>
-      <p className='result'>{result}</p>
-
+      <p className="result">{result}</p>
     </>
-  )
+  );
 }
-
-export default App
