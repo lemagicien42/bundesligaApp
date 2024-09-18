c3cd22cdd907a06640c6f1c056912efef13cf7e0
lemagicien42 <totosuper40@gmail.com>
2024-09-18 02:17:13 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,53 @@ function App() {
     fetch("https://api.openligadb.de/getmatchdata/bl1/2024")
       .then(response => response.json())
       .then(data => setMatches(data))
+      .catch(error => console.error("Error", error));
+  }, []);
+
+  const handleSearch = () => {
+    const foundMatch = matches.find(match => 
+      (match.team1.teamName.toLowerCase().includes(team1.toLowerCase()) &&
+       match.team2.teamName.toLowerCase().includes(team2.toLowerCase())) ||
+      (match.team1.teamName.toLowerCase().includes(team2.toLowerCase()) &&
+       match.team2.teamName.toLowerCase().includes(team1.toLowerCase()))
+    );
+    
+    if (foundMatch) {
+      const homeTeam = foundMatch.team1.teamName;
+      const awayTeam = foundMatch.team2.teamName;
+      const score = `${foundMatch.goals[1].scoreTeam1}:${foundMatch.goals[1].scoreTeam2}`;
+      const date = `${foundMatch.matchDateTime}`;
+
+      setResult(`Match found:
+      Date: ${date}\n
+      das Spiel findet in 
+      ${homeTeam} vs ${awayTeam}\n
+      Score: ${score}`);
+    } else {
+      setResult( " No match");
+    }
+  };
+
+  return (
+    <>
+      <h1>Bundesliga Match Finder</h1>
+      <div className='inputStyle'>
+        <input
+          type="text"
+          value={team1}
+          onChange={(e) => setTeam1(e.target.value)}
+          placeholder="Enter First Team Name"
+        />
+        <input
+          type="text"
+          value={team2}
+          onChange={(e) => setTeam2(e.target.value)}
+          placeholder="Enter Second Team Name"
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <p className='result'>{result}</p>
+
     </>
   )
 }
